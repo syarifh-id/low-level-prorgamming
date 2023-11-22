c303795ad4cb09c526386b185e44af6edb6dc9b1
syarifh-id <125syarifh@gmail.com>
2023-11-22 17:26:03 +0700

    escape sequnce

--- hello.c
+++ hello.c
@@ -5,7 +5,12 @@
 int main(){
   printf("hello world");
   // To insert a new line, you can use the \n character
+  // Two \n characters after each other will create a blank line:
   printf("\nIts new line on bottom");
 
 
 }
+// another escape sequence
+// \t 	Creates a horizontal tab 	
+// \\ 	Inserts a backslash character (\) 	
+// \" 	Inserts a double quote character 	
