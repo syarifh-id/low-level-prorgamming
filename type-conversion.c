72f52329b8a62160899cb7baaeb72d95ced2b64b
syarifh-id <125syarifh@gmail.com>
2023-12-04 21:06:50 +0700

    manual conversion

--- type-conversion.c
+++ type-conversion.c
@@ -14,7 +14,18 @@ int main()
 
 	printf("%i", myInt); // 9 
 
+	// Manual conversion: int to float
+float sum = (float) 5 / 2;
 
+printf("%f", sum); // 2.500000 
+
+
+//also can add before variable
+int num1 = 5;
+int num2 = 2;
+float sum = (float) num1 / num2;
+
+printf("%f", sum); // 2.500000 
 
 	return 0;
 }
