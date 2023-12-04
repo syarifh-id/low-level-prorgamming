26b924ac7144ae7626337cf72b0949062cfaf117
syarifh-id <125syarifh@gmail.com>
2023-12-04 20:59:10 +0700

    type conversion

---
+++ type-conversion.c
@@ -0,0 +1,21 @@
+//type conversion
+#include <stdio.h>
+int main()
+{
+	
+	// Automatic conversion: int to float
+
+	float myFloat = 9;
+
+	printf("%f", myFloat); // 9.000000 
+
+	// Automatic conversion: float to int
+	int myInt = 9.99;
+
+	printf("%i", myInt); // 9 
+
+
+
+	return 0;
+}
+
