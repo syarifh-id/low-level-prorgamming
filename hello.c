9346d27cae9c3766d491d6bc9270c7c7fb13ad30
syarifh-id <125syarifh@gmail.com>
2024-01-28 17:31:56 +0700

    add some explains

--- hello.c
+++ hello.c
@@ -2,12 +2,12 @@
 
 #include <stdio.h>
 
-int main(){
+int main(void){ //this mean the function  tidak menerima argument
   printf("hello world");
   // To insert a new line, you can use the \n character
   // Two \n characters after each other will create a blank line:
   printf("\nIts new line on bottom");
-
+return 0; // menandakan bahwa fungsi ini selesai
 
 }
 /* 
