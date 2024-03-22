e803aac224a52da5b99a3052111e280f09bdd4ee
syarifh-id <125syarifh@gmail.com>
2024-03-22 17:17:04 +0700

    fixing compiler

--- C++/latihan1.cpp
+++ C++/latihan1.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 
+use namespace std;
 int main(){
 
-string nama = "syarifh";
+
+string nama;
+
 int umur;
 double tinggi;
 char rate;
 bool married;
+nama = "syarif";
+
+cout << nama;
 
 }
 
---
+++ C++/tempCodeRunnerFile.cpp
@@ -0,0 +1,5 @@
+
+
+std::cout << nama;
+
+}
